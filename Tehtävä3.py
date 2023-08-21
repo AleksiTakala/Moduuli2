ed367f42fbffacdeb68269fc1e2d6d317313b291
AleksiTakala <at.345688@gmail.com>
2023-08-21 12:03:18 +0300

    Tehtävä 3

---
+++ Tehtävä3.py
@@ -0,0 +1,11 @@
+import math
+Kanta = input('Anna kanta: ')
+Korkeus = input('Anna korkeus: ')
+
+Kantaa = float(Kanta)
+Korkeusx = float(Korkeus)
+
+Pinta = Kantaa*Korkeusx
+Piiri = Korkeusx * 4
+
+print("Suorakulmion pinta-ala on " ,Pinta , "Ja piiri on " , Piiri)
