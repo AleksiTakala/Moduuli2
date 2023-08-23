1e46b1b423ef3f8c32c6bc7c8433e1ca34b45463
AleksiTakala <at.345688@gmail.com>
2023-08-23 15:05:42 +0300

    T4

---
+++ Thetävä 4.py
@@ -0,0 +1,21 @@
+import math
+print("Anna kolme kokonais lukua.")
+Luku1 = input()
+Luku2= input()
+Luku3 = input()
+
+a = float(Luku1)
+b = float(Luku2)
+c = float(Luku3)
+
+Yhteen = a+b+c
+kerto = a*b*c
+keski = Yhteen/3
+
+print(Yhteen, kerto, keski)
+
+
+
+
+
+
